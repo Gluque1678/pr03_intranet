888360e88e4a92d13d69776f041a5196ca7b9b20
Gluque1678 <92076.joan23@fje.edu>
2015-11-16 11:08:43 +0100

    proyecto2

---
+++ funcionresaltar.js
@@ -0,0 +1,14 @@
+function resalta(elemento) {
+	switch(elemento.style.borderColor)}{
+	case 'silver':
+	case 'silver silver silver silver':
+	case '#c0c0c0':
+	elemento.style.borderColor = 'black' ;
+	break;
+	case 'black':
+	case 'black black black':
+	case '#000000'
+	elemento.style.borderColor = 'silver';
+	break;
+	}
+}
